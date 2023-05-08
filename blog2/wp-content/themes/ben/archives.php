36a7e91d3d85882f84f54a0be5ee288141d6726f
Ben Schultz <bens4lsu@gmail.com>
2023-05-08 16:44:54 -0500

    pdo

--- blog2/wp-content/themes/ben/archives.php
+++ blog2/wp-content/themes/ben/archives.php
@@ -1,4 +1,11 @@
-<?php get_header(); ?>
+<?php get_header(); 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+?>
 
 <div id="content" class="widecolumn">
 
