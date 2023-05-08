f40c2d655051bd2a7a5379c10e52ffd766442890
Ben Schultz <bens4lsu@gmail.com>
2023-05-08 16:40:27 -0500

    pdo

--- blog2/wp-content/themes/ben/archives.php
+++ blog2/wp-content/themes/ben/archives.php
@@ -3,15 +3,12 @@
 <div id="content" class="widecolumn">
 
 <?php
-	ini_set ('display_errors',1);
-	$db_connection = mysql_connect ('162.209.78.149', 'theskinn_user', 'dN)h~H5AzrzLsT552d37880#9305') or die (mysql_error());
-	$db_select = mysql_select_db ('sites_theskinny') or die (mysql_error());
-
-	$sqlList = mysql_query ("SELECT `post_date`, `post_title`, `ID` FROM `wpben_posts` where ID <> 175 and ID <> 177 and post_type = 'post' and post_status = 'publish' ORDER BY `post_date` DESC");
+	require_once ("../../../db-pdo.class.php");
+	$sqlList = $db->prepare ("SELECT `post_date`, `post_title`, `ID` FROM `wpben_posts` where post_type = 'post' and post_status = 'publish' ORDER BY `post_date` DESC");
 
 	$prevMonthCode = '';
 	$firstFlag = false;
-	while ($row = mysql_fetch_array ($sqlList)){
+	while ($row = $db->selectRowsArrayAssoc($sqlList)){
 		$intDate = strtotime ($row[post_date]);
 
 		$currMonthCode = date('MY',$intDate);
