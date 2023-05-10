1d6e911dd4d7f2cd395220a3f9abdf39e1ffb5eb
Ben Schultz <bens4lsu@gmail.com>
2023-05-10 15:51:29 -0500

    remove site-local dailyphotos

--- dailyphoto/2007/noimage.php
+++
@@ -1,3 +0,0 @@
-<div class="entry"><div class="post">You have requested a document for which there is no image.  Please click the following link, or follow a link from the callendar below.
-<br><br>
-<a href="/dailyphoto">Latest Document</a>
