83f352fa052a01152dacd25572405507a28754cb
Ben Schultz <bens4lsu@gmail.com>
2023-05-10 14:54:59 -0500

    point to dynamic.theskinnyonbenny.com

--- dailyphoto/index.php
+++ dailyphoto/index.php
@@ -1,40 +1,41 @@
-<html><head>
+<html>
+<head>
 <?php include ('../tracking.php') ?>
 
-<SCRIPT language="JavaScript">
+<title>Ben's daily photos on theskinnyonbenny.com</title>
+<meta name=description content="Web Page for Ben Schultz, Baton Rouge, LA ">
+<meta name=keywords content="Photo, Photoblog, Humor, Images">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="PUBLIC">
+<meta property="og:image" content="<?php print $fullimagepath; ?>" />
+<meta property="og:description" content="" />
+<link rel="stylesheet" type="text/css" href="https://theskinnyonbenny.com/style.css">
+<link rel="shortcut icon" href="/img/headers/favicon.ico" type="image/x-icon" />
+<script language="javascript" src="https://theskinnyonbenny.com/menu.js"></script>
+<style type="text/css" media="screen">
+	body { background: url("https://theskinnyonbenny.com/blog2/wp-content/themes/ben/images/bgcolor.jpg"); }
+	#page { background: url("https://theskinnyonbenny.com/blog2/wp-content/themes/ben/images/bg.jpg") repeat-y top; border: none; }
+	#header { background: url("https://theskinnyonbenny.com/img/headers/header-theskinnyonbenny.jpg") no-repeat bottom center; }
+	#footer { background: url("https://theskinnyonbenny.com/blog2/wp-content/themes/ben/images/footer.jpg") no-repeat bottom; border: none;}
+	#header 	{ margin: 0 !important; margin: 0 0 0 1px; padding: 1px; height: 198px; width: 758px; }
+	#headerimg 	{ margin: 7px 9px 0; height: 192px; width: 740px; }
+</style>
+</head>
+<body>
+<div class="topleft"><img src="https://theskinnyonbenny.com/img/BenPurpleGold.JPG"></div>
+<div class="sb2"><?php include("../menu2.php"); ?></div>
 
-<?php
+<div id="page">
+	<div id="header">
+		<div id="headerimg"></div>
+	</div>
 
-$now_array = getdate();
+	<div id="content" class="widecolumn">
+	    <iframe src="https://dynamic.theskinnyonbenny.com/dp" style="border:none;"></iframe>
+	</div>
 
-if ($handle = opendir('./'.$now_array["year"].'/')) {
-	while (($file = readdir($handle)) !== false) {
-    if (substr (mb_convert_case($file,MB_CASE_LOWER), -4, 4) == ".jpg")
-       $files[]=$file;
-    }
-}
+	<?php include("../indexFooter.php");	?>
 
-if (empty($files)){
-	$now_array['year']--;
-	if ($handle = opendir('./'.$now_array['year'].'/')) {
-		while (($file = readdir($handle)) !== false) {
-		if (substr (mb_convert_case($file,MB_CASE_LOWER), -4, 4) == ".jpg")
-		   $files[]=$file;
-		}
-	}
-}
-
-rsort($files);
-$i = 0;
-while (substr($files[$i],0,4) > $now_array["year"] || substr($files[$i],0,4) == $now_array["year"] && substr($files[$i],4,2) > $now_array["mon"] || substr($files[$i],0,4) == $now_array["year"] && substr($files[$i],4,2) == $now_array["mon"] && substr($files[$i],6,2) > $now_array["mday"])
-	$i++;
-$year = substr($files[$i],0,4);
-$month = substr($files[$i],4,2);
-$day = substr($files[$i],6,2);
-echo "window.location=\"https://theskinnyonbenny.com/dailyphoto/".$year."/page.php?year=".$year."&month=".$month."&day=".$day."\"";
-
-?>
-
-</SCRIPT>
-</HEAD>
-<body></body></html>
+</div>
+</body>
+</html>
