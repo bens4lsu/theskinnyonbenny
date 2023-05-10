a957194bc0c411d16d19771d4cc2a9932456647f
Ben Schultz <bens4lsu@gmail.com>
2023-05-10 11:05:19 -0500

    trying to fix category archive

--- blog2/wp-content/themes/ben/cat_archive.php
+++ blog2/wp-content/themes/ben/cat_archive.php
@@ -15,38 +15,38 @@
 
     <?php
 
-                $subBaseURL = $baseURL."sortby=categories&category_id=";
-                $category_id = $_GET["category_id"];
-                $cats = get_categories('sort_column=name&optioncount=1');		
-                if (is_array($cats) && count($cats)) { ?>
-                    <p class="index_expand">
-                        <a title="Expand all posts by categories" href="<?=$subBaseURL?>all">Expand all</a>
-                    </p>
-                    <ul class="index">
-                        <?php foreach ($cats as $cat) { ?>
-                            <li id="cat-<?=$cat->cat_ID?>" class="cat-item">
-                                <a title="View all posts filed under <?=$cat->name?>" href="<?=$subBaseURL.$cat->cat_ID?>#cat-<?=$cat->cat_ID?>"><?=$cat->name?></a> <span>(<?=$cat->count?>)</span>
-                            </li>
-                            <? if ($category_id == $cat->cat_ID || $category_id == "all") {
-                                global $post;
-                                $posts = get_posts('category='.$cat->cat_ID.'&numberposts='.$cat->count);
-                                if (is_array($posts) && count($posts)) { ?>
-                                    <ul class="index_posts">
-                                        <? foreach($posts as $post) { 
-                                            $author = get_userdata($post->post_author); ?>
-                                            <li>
-                                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> by <?=$author->first_name?> <?=$author->last_name?> 
-                                                <p>
-                                                    <?=substr(strip_tags($post->post_content), 0, $nbSumupChars)?><?=strlen($post->post_content)>$nbSumupChars ? "..." : ""?>
-                                                </p>
-                                            </li>
-                                            <? } ?>
-                                    </ul>
-                                <? } ?>
-                            <? } ?>
+        $subBaseURL = $baseURL."sortby=categories&category_id=";
+        $category_id = $_GET["category_id"];
+        $cats = get_categories('sort_column=name&optioncount=1');		
+        if (is_array($cats) && count($cats)) { ?>
+            <p class="index_expand">
+                <a title="Expand all posts by categories" href="<?=$subBaseURL?>all">Expand all</a>
+            </p>
+            <ul class="index">
+                <?php foreach ($cats as $cat) { ?>
+                    <li id="cat-<?=$cat->cat_ID?>" class="cat-item">
+                        <a title="View all posts filed under <?=$cat->name?>" href="<?=$subBaseURL.$cat->cat_ID?>#cat-<?=$cat->cat_ID?>"><?=$cat->name?></a> <span>(<?=$cat->count?>)</span>
+                    </li>
+                    <? if ($category_id == $cat->cat_ID || $category_id == "all") {
+                        global $post;
+                        $posts = get_posts('category='.$cat->cat_ID.'&numberposts='.$cat->count);
+                        if (is_array($posts) && count($posts)) { ?>
+                            <ul class="index_posts">
+                                <? foreach($posts as $post) { 
+                                    $author = get_userdata($post->post_author); ?>
+                                    <li>
+                                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> by <?=$author->first_name?> <?=$author->last_name?> 
+                                        <p>
+                                            <?=substr(strip_tags($post->post_content), 0, $nbSumupChars)?><?=strlen($post->post_content)>$nbSumupChars ? "..." : ""?>
+                                        </p>
+                                    </li>
+                                    <? } ?>
+                            </ul>
                         <? } ?>
-                    </ul>
-                <? } 
+                    <? } ?>
+                <? } ?>
+            </ul>
+        <? } 
     ?>
 			
 	
