49bb614ef528ef8087ec23eeb88948cb461508c2
Ben Schultz <bens4lsu@gmail.com>
2023-05-11 15:16:10 -0500

    still working on cat-archive.php

--- blog2/wp-content/themes/ben/cat_archive.php
+++ blog2/wp-content/themes/ben/cat_archive.php
@@ -2,28 +2,28 @@
 
 <div id="content" class="widecolumn">
 
-<?php
-	$page_id = (integer)$_GET['page_id'];
-	$baseURL = '/blog2/cat_archives?';
- 	$sortby = "categories"; 
-	if (isset($_GET['sortby'])) $sortby = $_GET['sortby'];
-	$nbSumupChars = 200;
-?>      
-
-
-    <h2>Blog Index</h2>
+   
+    <h2>Index by Category</h2>
 
     <?php
-
-        $subBaseURL = $baseURL."sortby=categories&category_id=";
-        $category_id = $_GET["category_id"];
         $cats = get_categories('sort_column=name&optioncount=1');		
-
-   ?>
-			
-	
-
-
+        
+        if (is_array($cats) && count($cats)) { 
+            
+            
+            foreach ($cats as $cat) { 
+                print_r($cat);
+                echo '<h2>cat name</h2>';
+                $posts = get_posts('category='.$cat->cat_ID.'&numberposts='.$cat->count);
+                if (is_array($posts) && count($posts)) { 
+                    echo  '<ul class="index_posts">';
+                    foreach($posts as $post) { 
+                        //$author = get_userdata($post->post_author);
+                        echo '<li><a href="'.the_permalink()'">'.the_title().'</a></li>'; 
+                    }
+                }
+            }
+    ?>
 </div>
 
 <?php get_footer(); ?>
